b32e709c6d05e39b6df57052fe8bdcdb83aa2056
buddycore <social@buddycore.com>
2016-08-23 04:42:12 +0100

    Modified both markup and style for sidebar

--- sidebar.bak.php
+++ sidebar.bak.php
@@ -1,6 +1,17 @@
 <aside class="global">
     <div class="widgets">
-        <div class="widget tabs thirds">
+        <?php
+        /*
+        <div class="widget advert">
+            <h2>Buy This Theme</h2>
+            <p>WordPress <i class="fa fa-plus" aria-hidden="true"></i> BuddyPress <i class="fa fa-plus" aria-hidden="true"></i> BBPress</p>
+            <p class="text">It's fully responsive too!</p>
+            <p><button>Buy Now for &pound;10.00</button></p>
+            <p class="under">or <a href="#">Watch an Introduction Video</a></p>
+        </div>
+        */
+        ?>
+        <div class="widget tabs">
             <ul class="tabs-nav">
                 <?php if(!is_user_logged_in()) : ?>
                     <li class="login"><a href="#t1"><span>Login</span></a></li>
@@ -9,6 +20,9 @@
                 <?php endif; ?>
                 <li class="categories"><a href="#t2"><span>Categories</span></a></li>
                 <li class="tags"><a href="#t3"><span>Tag Cloud</span></a></li>
+                <li class="bp-members"><a href="#t4"><span>Members</span></a></li>
+                <li class="bp-groups"><a href="#t5"><span>Groups</span></a></li>
+                <li class="social"><a href="#t6"><span>Social</span></a></li>
             </ul>
             <div class="tabs-panel login" id="t1">
                 <?php if(!is_user_logged_in()) : ?>
@@ -26,7 +40,6 @@
                 </form>
                 <?php else : ?>
                 <div class="summary">
-                    <?php // IF REMOVED YOU'll NEED TO MODIFY THE SIDEBAR STYLES in scss/templates/sidebar.scss ?>
                     <h3>Your Summary</h3>
                     <p>Welcome back <a href="<?php echo bp_loggedin_user_domain(); ?>"><?php echo bp_core_get_user_displayname(bp_loggedin_user_id()); ?></a>, you have posted <strong>xx status updates</strong> to your profile and <strong>xx group updates</strong>.</p>
                     <p>You have also created <strong>xx sites</strong> with a total of <strong>xx posts</strong> and you have created <strong>xx topics</strong> in the forums with <strong>xx replies</strong> in total.</p>
@@ -70,6 +83,31 @@
                 ); ?>
                 <?php wp_tag_cloud($args); ?>
                 </div>
+            </div>
+            <div class="tabs-panel bp-members" id="t4">
+                <h3>Members</h3>
+                <ul>
+                    <?php for($i = 0; $i < 16; $i++) : ?>
+                    <li><a href="#"><img src="<?php echo asset_url('img/default/member.png'); ?>" alt="Member"></a></li>
+                    <?php endfor; ?>
+                </ul>
+            </div>
+            <div class="tabs-panel bp-groups" id="t5">
+                <h3>Groups</h3>
+                <ul>
+                    <?php for($i = 0; $i < 16; $i++) : ?>
+                    <li><a href="#"><img src="<?php echo asset_url('img/default/group.png'); ?>" alt="Member"></a></li>
+                    <?php endfor; ?>
+                </ul>
+            </div>
+            <div class="tabs-panel social" id="t6">
+                <h3>Social Media</h3>
+                <ul>
+                    <li class="twitter"><a href="#">Follow us @buddycore</a></li>
+                    <li class="facebook"><a href="#">Visit our Facebook</a></li>
+                    <li class="youtube"><a href="#">Subscribe on YouTube</a></li>
+                    <li class="github"><a href="#">Code on GitHub</a></li>
+                </ul>
             </div>
         </div>
     </div><!-- WIDGETS -->
