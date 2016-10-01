b7aab8216ab26f5775c127635b1d9bfd3688c991
buddycore <social@buddycore.com>
2016-10-01 19:22:08 +0100

    Profile header now displayed on single activity item

--- buddypress/activity/single/home.php
+++ buddypress/activity/single/home.php
@@ -2,6 +2,14 @@
 <div class="main-col">
 	<div id="buddypress">
 		<?php do_action('template_notices'); ?>
+		
+		<?php
+		if(bp_displayed_user_use_cover_image_header()) :
+			bp_get_template_part('members/single/cover-image-header');
+		else :
+			bp_get_template_part('members/single/member-header');
+		endif;
+		?>
 
 		<div class="activity no-ajax">
 			<?php if(bp_has_activities('display_comments=threaded&show_hidden=true&include=' . bp_current_action())) : ?>
